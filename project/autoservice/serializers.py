c2a6fdf4c851dabb348d62fd6fab5c8d64b206ec
FanatikLeo2 <leonfar@yandex.ru>
2024-10-24 09:42:37 -0300

    added views

--- project/autoservice/serializers.py
+++ project/autoservice/serializers.py
@@ -1,49 +1,69 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User, Group
-from .models import *
+from .models import (
+    MachineModel,
+    EngineModel,
+    TransmissionModel,
+    SteeringAxleModel,
+    DrivingAxleModel,
+    ServiceCompany,
+    Machine,
+    MaintenanceType,
+    Maintenance,
+    FailureNode,
+    RecoveryMethod,
+    Complaint)
 
-class MachineModelSerializer(serializers.Modelserializer):
+
+class MachineModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = MachineModel
         fields = ['id', 'name', 'description']
 
-class EngineModelSerializer(serializers.Modelserializer):
+
+class EngineModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = EngineModel
         fields = ['id', 'name', 'description']
 
-class TransmissionModelSerializer(serializers.Modelserializer):
+
+class TransmissionModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = TransmissionModel
         fields = ['id', 'name', 'description']
-    
-class SteeringAxleModelSerializer(serializers.Modelserializer):
+
+
+class SteeringAxleModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = SteeringAxleModel
         fields = ['id', 'name', 'description']
 
-class DrivingAxleModelSerializer(serializers.Modelserializer):
+
+class DrivingAxleModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = DrivingAxleModel
         fields = ['id', 'name', 'description']
 
-class ServiceCompanySerializer(serializers.Modelserializer):
+
+class ServiceCompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceCompany
         fields = ['id', 'name', 'description']
 
-class ClientSerializer(serializers.Modelserializer):
+
+class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'groups']
 
-class MachineSerializer(serializers.Modelserializer):
+
+class MachineSerializer(serializers.ModelSerializer):
     machine_model = MachineModelSerializer()
     engine_model = EngineModelSerializer()
     transmission_model = TransmissionModelSerializer()
     steering_axle_model = SteeringAxleModelSerializer()
     driving_axle_model = DrivingAxleModelSerializer()
-    date_of_shipment = serializers.DataField()
+    date_of_shipment = serializers.DateField()
     client = ClientSerializer()
     service_company = ServiceCompanySerializer()
 
@@ -52,48 +72,53 @@ class MachineSerializer(serializers.Modelserializer):
         fields = ['id', 'machine_factory_num', 'machine_model',
                   'engine_factory_num', 'engine_model', 
                   'transmission_factory_num', 'transmission_model', 
-                  'steering_axle_model_num', 'steering_axle_model', 
-                  'driving_axle_model_num', 'driving_axle_model_model', 
+                  'steering_axle_factory_num', 'steering_axle_model',
+                  'driving_axle_factory_num', 'driving_axle_model',
                   'delivery_contract_num', 'date_of_shipment', 
                   'customer', 'delivery_address', 
                   'equipment', 'client', 
                   'service_company',
                 ]
 
-class MaintainanceTypeSerializer(serializers.Modelserializer):
+
+class MaintenanceTypeSerializer(serializers.ModelSerializer):
     class Meta:
-        mosdel = MaintainanceType
+        model = MaintenanceType
         fields = ['id', 'name', 'description']
 
-class MaintainanceSerializer(serializers.Modelserializer):
+
+class MaintenanceSerializer(serializers.ModelSerializer):
     machine = MachineSerializer()
-    maintanance_type = MaintainanceTypeSerializer()
-    event_date = serializers.DataField()
-    operating_time = serializers.DataField()
+    maintenance_type = MaintenanceTypeSerializer()
+    event_date = serializers.DateField()
+    order_date = serializers.DateField()
     service_company = ServiceCompanySerializer()
 
     class Meta:
-        model = Maintainance
-        fields = ['id', 'machine', 'maintanance_type', 'event_date', 
+        model = Maintenance
+        fields = ['id', 'machine', 'maintenance_type', 'event_date',
                   'operating_time', 'order_id', 'order_date', 
                   'service_company'
                 ]
-        
-class FailureNodeSerializer(serializers.Modelserializer):
+
+
+class FailureNodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = FailureNode
         fields = ['id', 'name', 'description']
 
-class RecoveryMethodSerializer(serializers.Modelserializer):
+
+class RecoveryMethodSerializer(serializers.ModelSerializer):
     class Meta:
         model = RecoveryMethod
         fields = ['id', 'name', 'description']
 
-class ComplaintSerializer(serializers.Modelserializer):
-    refusal_date = serializers.DataField()
+
+class ComplaintSerializer(serializers.ModelSerializer):
+    refusal_date = serializers.DateField()
     failure_node = FailureNodeSerializer()
     recovery_method = RecoveryMethodSerializer()
-    recovery_date = serializers.DataField()
+    recovery_date = serializers.DateField()
     machine = MachineSerializer()
     service_company = ServiceCompanySerializer()
 
@@ -101,5 +126,5 @@ class ComplaintSerializer(serializers.Modelserializer):
         model = Complaint
         fields = ['id', 'refusal_date', 'operating_time', 'failure_node',
                   'failure_description', 'recovery_method',
-                  'spare_parts_used', 'spare_parts_used', 'equipment_downtime',
-                  'machine', 'service_company']
+                  'spare_parts_used', 'recovery_date',
+                  'equipment_downtime', 'machine', 'service_company']
---
+++ project/autoservice/urls.py
@@ -0,0 +1,35 @@
+from rest_framework import routers
+from django.urls import path, include
+from .views import (
+    MachinesViewSet,
+    MaintenancesViewSet,
+    ComplaintsViewSet,
+    UserViewSet,
+    MachineModelViewSet,
+    SteeringAxleModelViewSet,
+    DrivingAxleModelViewSet,
+    EngineModelViewSet,
+    TransmissionModelViewSet,
+    RecoveryMethodViewSet,
+    FailureNodeViewSet,
+    MaintenanceTypeViewSet,
+    ServiceCompanyViewSet)
+
+router = routers.DefaultRouter()
+router.register(r'machine', MachinesViewSet)
+router.register(r'maintenance', MaintenancesViewSet)
+router.register(r'complaint', ComplaintsViewSet)
+router.register(r'user', UserViewSet)
+router.register(r'machine_model', MachineModelViewSet)
+router.register(r'steering_axle_model', SteeringAxleModelViewSet)
+router.register(r'driving_axle_model', DrivingAxleModelViewSet)
+router.register(r'engine_model', EngineModelViewSet)
+router.register(r'transmission_model', TransmissionModelViewSet)
+router.register(r'recovery_method', RecoveryMethodViewSet)
+router.register(r'failure_node', FailureNodeViewSet)
+router.register(r'maintenance_type', MaintenanceTypeViewSet)
+router.register(r'service_company', ServiceCompanyViewSet)
+
+urlpatterns = [
+    path('autoservice/', include(router.urls))
+]
